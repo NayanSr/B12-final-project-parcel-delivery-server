7da1cd700915da1ad1239200b2f413a1914398c5
Nayan Suter <nayankbps@gmail.com>
2026-05-05 11:46:45 +0600

    find a single data

--- index.js
+++ index.js
@@ -52,6 +52,13 @@ async function run() {
 
     })
 
+    app.get('/parcels/:id', async(req,res)=>{
+      const id= req.params.id;
+      const query= {_id: new ObjectId(id)};
+      const result= await parcelCollection.findOne(query);
+      res.send(result)
+    })
+
     app.post('/parcels', async(req,res)=>{
       const parcel= req.body;
       parcel.createdAt= new Date();
