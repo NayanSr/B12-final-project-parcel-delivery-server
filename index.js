95401f74c50c1bbd39459f1d045de7e1df0f0a77
Nayan Suter <nayankbps@gmail.com>
2026-05-15 22:35:24 +0600

    implement get a user using search query(name or email) 65_6

--- index.js
+++ index.js
@@ -105,10 +105,25 @@ next();
     });
 
     app.get("/users",verifyFBToken, async (req, res) => {
-      const cursor = userCollection.find();
+      const searchText= req.query.searchText;
+      const query={};
+      if(searchText){
+        // query.displayName= {$regex: searchText, $options: 'i'}
+        query.$or=[
+          {displayName:{$regex:searchText, $options:'i'}},
+          {email:{$regex:searchText, $options:'i'}}
+        ]
+      }
+      const cursor = userCollection.find(query).sort({createdAt:-1}).limit(5);
       const result = await cursor.toArray();
       res.send(result);
     });
+
+
+
+
+
+
       // GET user using id
     app.get('/users/:id', async(req,res)=>{
       const id= req.params.id;
